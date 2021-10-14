6259677022ea125a3fe0ddf54ca47e46b01c1a28
yeonjiyeon <dalnim95@naver.com>
2021-10-14 21:21:28 +0900

    hi

--- com.example.book/src/test/java/com/example/book/springboot/domain/posts/PostsRepositoryTest.java
+++ com.example.book/src/test/java/com/example/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,8 +1,10 @@
 package com.example.book.springboot.domain.posts;
 
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class PostsRepositoryTest {
 }
